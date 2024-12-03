d3f4cb642934491bf141bbf444fa67c9fd86b1c1
Sisalio <sisalio_phene@yahoo.fr>
2024-12-03 22:07:36 +0100

    wording

--- app.vue
+++ app.vue
@@ -807,7 +807,7 @@ async function onSubmit(event) {
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
+          <h1>Welcome to Toll App</h1>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
       </template>
