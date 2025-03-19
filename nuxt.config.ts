e330abda8390acb894947b9a39ef7df0a39c9e1b
Sisalio <sisalio_phene@yahoo.fr>
2025-03-19 19:04:25 +0100

    step 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
   ssr: true,
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
